f4065b8bcd9ca2538eaa3ce4828199b0a677fbdb
Dhirendra Kumar <kushwahadk799@gmail.com>
2026-08-29 02:58:04 +0530

    add subnet starlink and streamflix

--- parrent module/variable.tf
+++ parrent module/variable.tf
@@ -18,3 +18,14 @@ variable "virtual_networks" {
 
   }))
 }
+
+variable "subnets" {
+  description = "Subnet configuration"
+
+  type = map(object({
+    name                 = string
+    resource_group_name  = string
+    virtual_network_name = string
+    address_prefixes     = list(string)
+  }))
+}
